23fd735d2f009c8e410bfd5e257a013a2e96e325
Tom Nicholson <tfwnicholson+1@gmail.com>
2021-09-14 04:12:30 +0900

    More tests around type checker

--- test/TyperSpec.hs
+++ test/TyperSpec.hs
@@ -20,24 +20,29 @@ NOW shouldBe :: (Show a, Eq a) => a -> a -> Expectation
 spec :: Spec
 spec = describe "Typer" $ do
     describe "checkTypes" $ do
-        it "can deal with scalar types" $ do 
-            checkType emptyTypes (CS (1::Float)) `shouldBe` TScalar
-        it "can deal with index types" $ do
-            checkType emptyTypes (CI (1::Int)) `shouldBe` TIndex
-        it "can deal with var types of type scalar" $ do
-            expectVarTypes TScalar
-        it "can deal with var types of type index" $ do
-            expectVarTypes TIndex
-        it "can deal with var types of type vector" $ do
-            expectVarTypes (TVector 3)
-        it "can deal with var types of type matrix" $ do
-            expectVarTypes (TMatrix  6 3)
-        it "can type pluses of scalars" $ do
-            checkType emptyTypes ((1.0 :: Float) |+ (2.0 :: Float)) `shouldBe` TScalar 
-        it "can type pluses of Index" $ do
-            checkType emptyTypes ((1::Int) |+ (2::Int)) `shouldBe` TIndex
-        it "can pluses of vectors" $ do
-            checkType emptyTypes [1::Float, 2] |+ [3::Float, 4] `shouldBe` TVector 2 
+        describe "raw types" $ do
+            it "can deal with scalar types" $ do 
+                checkType emptyTypes (CS (1::Float)) `shouldBe` TScalar
+            it "can deal with index types" $ do
+                checkType emptyTypes (CI (1::Int)) `shouldBe` TIndex
+            it "can deal with var types of type scalar" $ do
+                expectVarTypes TScalar
+            it "can deal with var types of type index" $ do
+                expectVarTypes TIndex
+            it "can deal with var types of type vector" $ do
+                expectVarTypes (TVector 3)
+            it "can deal with var types of type matrix" $ do
+                expectVarTypes (TMatrix  6 3)
+        describe "addition" $do 
+            it "can type pluses of scalars" $ do
+                checkType emptyTypes ((1.0 :: Float) |+ (2.0 :: Float)) `shouldBe` TScalar 
+            it "can type pluses of Index" $ do
+                checkType emptyTypes ((1::Int) |+ (2::Int)) `shouldBe` TIndex
+            it "can type pluses of vectors length 2" $ do
+                checkType emptyTypes ([1::Float, 2] |+ [3::Float, 4]) `shouldBe` TVector 2 
+            it "can type pluses of vectors length 1" $ do
+                checkType emptyTypes ([1::Float] |+ [2::Float]) `shouldBe` TVector 1
+            it "can type pluses of scalar and "
 
 main :: IO ()
 main = hspec spec
